a79131cd890026efbfe951c28f5ac0345e93e96e
jgaffuri <jgaf@proton.me>
2025-10-17 21:00:05 +0200

    a

--- src/process.py
+++ src/process.py
@@ -2,80 +2,23 @@
 from init_qgis import qgs, QgsLayoutExporter, QgsLayoutItemMap, QgsPrintLayout, QgsApplication, QgsProject, QgsMapSettings, QgsMapRendererParallelJob, QgsRasterFileWriter, QgsRectangle, QSize
 import sys
 
-'''
-def export_map_to_geotiff(project_path, output_path, scale, extent):
-    """Exporte la carte d'un projet QGIS au format GeoTIFF."""
-    project = QgsProject.instance()
-    project.read(project_path)
 
-    map_settings = QgsMapSettings()
-    map_settings.setLayers(project.mapLayers().values())
-    map_settings.setDestinationCrs(project.crs())
-    map_settings.setExtent(QgsRectangle(*extent))
-    #map_settings.setScale(scale)
-    map_settings.setOutputSize(QSize(800, 600))
-
-    job = QgsMapRendererParallelJob(map_settings)
-    job.start()
-    job.waitForFinished()
-
-    image = job.renderedImage()
-    writer = QgsRasterFileWriter(output_path)
-    writer.writeRaster(
-        image,
-        map_settings.extent().width(),
-        map_settings.extent().height(),
-        map_settings.extent(),
-        map_settings.destinationCrs()
-    )
-    print(f"Export terminé : {output_path}")
-'''
-
-
-def export_map_to_geotiff(project_path, output_path, extent, scale, dpi=96):
+def export_map_to_geotiff(project_path, output_path, extent, scale):
     """
     Exporte la carte d'un projet QGIS au format GeoTIFF.
 
     :param project_path: Chemin vers le fichier .qgz ou .qgs
     :param output_path: Chemin de sortie pour le GeoTIFF
     :param extent: Emprise sous forme de tuple (xmin, ymin, xmax, ymax)
-    :param dpi: Résolution en DPI (par défaut 96)
+    :param scale: Denominateur du facteur d'échelle
     """
     # Initialiser QGIS (nécessaire pour les scripts autonomes)
     qgs = QgsApplication([], False)
     qgs.initQgis()
 
-    #try:
     # Charger le projet
     project = QgsProject.instance()
     project.read(project_path)
-
-    # Configurer les paramètres de la carte
-    map_settings = QgsMapSettings()
-    map_settings.setLayers(project.mapLayers().values())
-    map_settings.setDestinationCrs(project.crs())
-    map_settings.setExtent(QgsRectangle(*extent))
-
-    #map_settings.setOutputSize(QSize(800, 600))  # Taille de sortie en pixels
-
-    # Créer un rendu de la carte
-    job = QgsMapRendererParallelJob(map_settings)
-    job.start()
-    job.waitForFinished()
-
-    '''
-    # Sauvegarder le rendu en GeoTIFF
-    image = job.renderedImage()
-    writer = QgsRasterFileWriter(output_path)
-    writer.writeRaster(
-        image,
-        map_settings.extent().width(),
-        map_settings.extent().height(),
-        map_settings.extent(),
-        map_settings.destinationCrs()
-    )
-    '''
-
 
     # Créer un layout
     layout = QgsPrintLayout(project)
@@ -83,7 +26,7 @@ def export_map_to_geotiff(project_path, output_path, extent, scale, dpi=96):
 
     # Ajouter une carte au layout
     map = QgsLayoutItemMap(layout)
-    map.setExtent(QgsRectangle(*extent)) # Remplace `extent` par tes coordonnées (xmin, ymin, xmax, ymax)
+    map.setExtent(QgsRectangle(*extent))
     map.setScale(scale)
     layout.addLayoutItem(map)
 
@@ -91,15 +34,8 @@ def export_map_to_geotiff(project_path, output_path, extent, scale, dpi=96):
     exporter = QgsLayoutExporter(layout)
     exporter.exportToImage(output_path, QgsLayoutExporter.ImageExportSettings())
 
-
-
     print(f"Export terminé : {output_path}")
 
-    #except Exception as e:
-    #    print(f"Erreur lors de l'export : {e}")
-
-    #finally:
-        # Désinitialiser QGIS
     qgs.exitQgis()
 
 
@@ -115,8 +51,8 @@ print(scales)
 
 scale = 1000000
 # (xmin, ymin, xmax, ymax)
-extent = (3700000, 2700000, 3850000, 2800000)
-export_map_to_geotiff("src/project.qgz", "tmp/aaa.png", extent, scale)
+extent = (3700000, 2700000, 3710000, 2710000)
+export_map_to_geotiff("src/project.qgz", "tmp/aaa.tiff", extent, scale)
 
 
 

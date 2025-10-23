de5f28314b3a788fd3e42ff0271c5c08c4131a90
jgaffuri <jgaf@proton.me>
2025-10-23 20:34:13 +0200

    a

--- src/qgis.py
+++ src/qgis.py
@@ -1,4 +1,6 @@
 
 import processing
+
+# see https://docs.qgis.org/3.40/en/docs/user_manual/processing_algs/qgis/rastertools.html#generate-xyz-tiles-directory
 processing.run("algorithm_id", {parameter_dictionary})
 

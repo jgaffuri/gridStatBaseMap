c2ea10e7e08710ea7b5b60d739b0d21a7a4e5a60
jgaffuri <jgaf@proton.me>
2025-10-28 11:44:20 +0100

    a

--- src/qgis.py
+++ src/qgis.py
@@ -112,8 +112,8 @@ def tile_from_qgis_project(output_folder, origin_point = [0, 0],
 
 tile_from_qgis_project(output_folder = "/home/juju/Bureau/tiles/",
     origin_point = [0, 6000000],
-    z_min = 9,
-    z_max = 12,
+    z_min = 2,
+    z_max = 4,
 )
 
 print("done")

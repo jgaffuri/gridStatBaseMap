d52a15f250be85a52889515a3c455c5cff4f4116
jgaffuri <jgaf@proton.me>
2025-11-19 14:35:41 +0100

    a

--- src/process.py
+++ src/process.py
@@ -15,6 +15,11 @@ from PyQt5.QtGui import QImage, QPainter, QColor
 from PyQt5.QtCore import QSize
 
 
+# fix limits
+# new elevation
+# new road - with seas ?
+
+
 
 def tile_from_qgis_project(project_path,
                            output_folder,
@@ -153,12 +158,13 @@ tile_from_qgis_project(
     project_path = "/home/juju/workspace/gridStatBaseMap/src/elevation.qgz",
     output_folder = "/home/juju/Bureau/tiles/",
     resolution0 = 114688,
-    extent = [0, 0, 6000000, 8000000],
+    #extent = [0, 0, 6000000, 8000000],
+    extent = [3945000, 2252000, 3947000, 2254000],
     #extent = [4060000, 2960000, 4080000, 2980000],
     origin_point = [0, 6000000],
     tile_size_px = 256, # 512 256
     z_min = 0,
-    z_max = 13,
+    z_max = 12,
     # Format_RGB16 Format_RGB32 Format_Grayscale16, # see on https://doc.qt.io/qt-6/qimage.html
     img_format = QImage.Format_RGB32,
 )

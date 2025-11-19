b637873383a4bad1fa22d8b772ed3b8298987b90
jgaffuri <jgaf@proton.me>
2025-11-19 15:20:12 +0100

    a

--- src/process.py
+++ src/process.py
@@ -158,13 +158,13 @@ tile_from_qgis_project(
     project_path = "/home/juju/workspace/gridStatBaseMap/src/elevation.qgz",
     output_folder = "/home/juju/Bureau/tiles/",
     resolution0 = 114688,
-    #extent = [0, 0, 6000000, 8000000],
-    extent = [3940000, 2250000, 3947000, 2257000],
+    extent = [0, 0, 6000000, 8000000],
+    #extent = [3940000, 2270000, 3947000, 2277000],
     #extent = [4060000, 2960000, 4080000, 2980000],
     origin_point = [0, 6000000],
     tile_size_px = 256, # 512 256
     z_min = 0,
-    z_max = 15,
+    z_max = 12,
     # Format_RGB16 Format_RGB32 Format_Grayscale16, # see on https://doc.qt.io/qt-6/qimage.html
     img_format = QImage.Format_RGB32,
 )

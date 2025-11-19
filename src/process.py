241324ee2b071d9547e3db78474d4fa115416646
jgaffuri <jgaf@proton.me>
2025-11-19 11:32:47 +0100

    a

--- src/process.py
+++ src/process.py
@@ -150,16 +150,16 @@ qgs.initQgis()
 
 
 tile_from_qgis_project(
-    project_path = "/home/juju/workspace/gridStatBaseMap/src/project.qgz",
+    project_path = "/home/juju/workspace/gridStatBaseMap/src/elevation.qgz",
     output_folder = "/home/juju/Bureau/tiles/",
     resolution0 = 114688,
     extent = [0, 0, 6000000, 6000000],
     #extent = [4060000, 2960000, 4080000, 2980000],
     origin_point = [0, 6000000],
-    tile_size_px = 512,
+    tile_size_px = 256, # 512 256
     z_min = 0,
-    z_max = 14,
-    img_format = QImage.Format_Grayscale16,
+    z_max = 13,
+    img_format = QImage.Format_RGB16, # Format_RGB16 Format_RGB32 Format_Grayscale16, # see on https://doc.qt.io/qt-6/qimage.html
 )
 
 # close
